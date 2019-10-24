6eba524e76d46b187a19cfbc98666a0fea5b6f8f
NDE TSAPI STEVE-ROLAND <56319959+Ster1402@users.noreply.github.com>
2019-10-24 10:52:17 +0100

    Update Compte.py

--- Compte.py
+++ Compte.py
@@ -1,5 +1,24 @@
 class Compte: #definition de notre classe Compte bancaire
   
-  def _init_(self) : 
-      
-      #définition de attribut de notre classe
+  def __init__(self) :   #définition du constructeur
+    
+    #création des attributs
+    self._code = ""
+    self._solde = 0.0
+    
+  def _get_code (self) :
+    return self._code
+  def _set_code (self , new_code ) :
+    self._code = new_code
+    
+  def _get_solde (self) : 
+    return self._solde
+  
+  def _verser (self, new_solde) :
+    self._solde = new_solde
+    
+    
+ #On signale a python que code et solde pointe vers des propriétés
+ #suivant la syntaxe : property( methode_accesseur, methode_mutateur, methode_suppression, methode_aide )
+ code = property( _get_code , _set_code )
+ solde = property( _get_code , _verser )
