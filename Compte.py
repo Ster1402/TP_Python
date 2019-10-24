c6b964dc1d514c8efad0cc4766492aabc2cbf8e9
NDE TSAPI STEVE-ROLAND <56319959+Ster1402@users.noreply.github.com>
2019-10-24 11:05:27 +0100

    Update Compte.py

--- Compte.py
+++ Compte.py
@@ -17,8 +17,10 @@ class Compte: #definition de notre classe Compte
   def _verser (self, new_solde) :
     self._solde = new_solde
     
+  def _set_solde(self, newSolde) :
+    self._verser( self,newSolde)
     
  #On signale a python que code et solde pointe vers des propriétés
  #suivant la syntaxe : property( methode_accesseur, methode_mutateur, methode_suppression, methode_aide )
  code = property( _get_code , _set_code )
- solde = property( _get_code , _verser )
+ solde = property( _get_code , _set_solde )
