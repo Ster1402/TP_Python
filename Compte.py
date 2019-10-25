b5e02982510f183d75358082faafb9ef6ecc32e3
NDE TSAPI STEVE-ROLAND <56319959+Ster1402@users.noreply.github.com>
2019-10-25 13:06:56 +0100

    Update Compte.py
    Classe permettant de gérer les comptes en banque

--- Compte.py
+++ Compte.py
@@ -1,26 +1,39 @@
-class Compte: #definition de notre classe Compte
-  
-  def __init__(self) :   #définition du constructeur
+class Compte: #création classe mère Compte bancaire
     
-    #création des attributs
-    self._code = ""
-    self._solde = 0.0
+    """Classe représentant le compte d'une personne"""
+    def __init__(self,n = 0,solde = 0.0):
+        self._numero = n
+        self._solde = solde
+        
+    def __str__(self):
+        """Méthode appelée lors d'une conversion de l'objet en chaîne"""
+        
+        return "Code compte : {0} \nSolde : {1}\n".format(self._numero, self.solde)
     
-  def _get_code (self) :
-    return self._code
-  def _set_code (self , new_code ) :
-    self._code = new_code
+    def _get_numero(self):
+        print("Votre numéro de compte est : {0} \n".format(self._numero))
+        return self._numero
     
-  def _get_solde (self) : 
-    return self._solde
-  
-  def _verser (self, new_solde) :
-    self._solde = new_solde
+    def _get_solde(self):
+        print("Votre solde est de {0} FCFA \n".format(self._numero))
     
-  def _set_solde(self, newSolde) :
-    self._verser( self,newSolde)
+    def _set_solde(self,new_solde):
+        print("Votre nouveau solde est de {0} FCFA \n".format(new_solde))
+        self._solde = new_solde
     
- #On signale a python que code et solde pointe vers des propriétés
- #suivant la syntaxe : property( methode_accesseur, methode_mutateur, methode_suppression, methode_aide )
- code = property( _get_code , _set_code )
- solde = property( _get_code , _set_solde )
+    def infoSolde(self):
+        print("Votre solde est de {0} FCFA ! \n".format(self._solde))
+
+    def versement(self,add):
+        self._solde += add
+        print("Versement de {0} FCFA éffectué ! \n".format(add))
+        self.infoSolde()
+    
+    def retrait(self,moins):
+        self._solde -= moins
+        print("Le montant débité est de {0} FCFA\n".format(moins))
+        self.infoSolde()
+    
+    solde = property(_get_solde, _set_solde)    
+    numero = property(_get_numero)
+
