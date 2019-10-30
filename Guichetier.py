ae77cb3e9aadecb1d664bf006852173ca7309bc9
NDE TSAPI STEVE-ROLAND <56319959+Ster1402@users.noreply.github.com>
2019-10-30 22:41:37 +0100

    Update Guichetier.py

--- Guichetier.py
+++ Guichetier.py
@@ -4,23 +4,25 @@ class Guichetier(Personne): #Sous-Classe Guichetier ayant comme classe mere Pers
 
     # Notre méthode constructeur
     
-     def __init__(self):
+    def __init__(self):
      
         #Constructeur de notre classe
-
-        self._Personne = Personne()
+        # On appelle explicitement le constructeur de Personne :
+        Personne.__init__(self)
 
     def affSolde(self):
         print("Solde disponible est de {0} FCFA : Protected".format(compte.solde))
 
-    def versement(self,compte,15000):
-        # Un montant de 15000 va etre retirer
-        print("Versement de {0} FCFA : Protected".format(15000))
+    def versement(self,compte,mt):
+        # Un montant de 15000 va etre ajouter au compte du client
+        compte.versement(mt)
+        print("Versement de {0} FCFA : Protected".format(mt))
         self.affSolde(compte)
     
-    def retrait(self,Compte,7000):
+    def retrait(self,compte,mt):
         """ ici, un test va etre effectuer dans la classe Controlleur pour savoir si le solde disponible est suffisant """
-        # Si "OUI" ... un retrait de 7000 par exemple est effectuer
-        print("montant débité de {0} FCFA: Protected".format(7000))
+        # Si "OUI" ... un retrait de 7000 par exemple est effectuer par le Guichetier avec la permission du Controleur
+        compte.retrait(mt)
+        print("montant débité de {0} FCFA: Protected".format(mt))
         
         self.affSolde()
