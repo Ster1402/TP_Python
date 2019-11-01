5478b1661977dc991898ce62004444d680cfbf25
TNES1403 <56323015+TNES1403@users.noreply.github.com>
2019-11-01 10:27:02 +0100

    Add files via upload

---
+++ Personne.py
@@ -0,0 +1,27 @@
+class Personne:
+    def __init__(self,matricule = " ",nom = " ",prenom = " "):
+        self._matricule = matricule
+        self._nom = nom
+        self._prenom = prenom
+        
+    def __str__(self): 
+        return "matricule :" + self._matricule + "\nnom :" + self._nom + "\nprenom :" + self._prenom
+    
+    def _get_matricule(self):
+        return self._matricule
+    def _set_matricule(self,m):
+        self._matricule = m
+    matricule = property(_get_matricule,_set_matricule)
+
+    def _get_nom(self):
+        return self._nom
+    def _set_nom(self,n):
+        self._nom = n
+    nom = property(_get_nom,_set_nom)
+    
+    def _get_prenom(self):
+        return self._prenom
+    def _set_prenom(self,p):
+        self._prenom = p
+    prenom = property(_get_prenom,_set_prenom)
+    
