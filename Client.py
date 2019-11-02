1c485321f406dbf390ae8dcab498ad3dc944d15d
TNES1403 <56323015+TNES1403@users.noreply.github.com>
2019-11-02 06:42:18 +0100

    Update Client.py

--- Client.py
+++ Client.py
@@ -8,6 +8,8 @@ class Client(Personne):
     def __init__(self):
         self.bank=Banque()
         self.compte=Compte()
+        #Le gestionnaire créé un compte lors de la création d'un client
+        self.bank.gestionnaire.ajoutCompte()
         self.dettes=0.0
         self.sommerecue=0.0
         self.salaire=240000 #cas d'un enseignent
@@ -17,20 +19,28 @@ class Client(Personne):
             Banque.guichetier.verser(client.compte,montant)
         else:
             print("solde insuffisant pour effectuer l'envoie")
+    
     def emprunt(self,salaire,montant):
-        if(salaire>=montant):
-            Banque.guichetier.verser(self.compte,montant)
+        if( ( self.bank.controleur.verifier(self.salaire,montant) ) && self.dettes == 0): #On vérifie que le salaire qu'il touche est suffisant
+            self.bank.guichetier.verser(self.sommerecue,montant) #Le guichetier de la banque lui remet alors l'argent
+            self.dettes = montant
         else:
             print("l'emprunt n'est pas possible")
+            
     def verser(self,client,montant):
-        Banque.guichetier.verser(client.compte,montant)
+        self.bank.guichetier.verser(client.compte,montant)
+        
     def retrait(self,client,montant):
-        if (Banque.controleur.verifier(self.compte.solde,montant)):
-            Banque.guichetier.verser(client.compte,montant)
+        if (self.bank.controleur.verifier(self.compte.solde,montant)):
+            self.bank.guichetier.verser(client.compte,montant)
         else:
             print("solde insuffisant pour effectuer le retrait")
             
-        
+    def passerAuCompteEpargne(self):
+        self.compte = Epargne()
+    
+    def passerAuCompteCourant(self):
+        self.compte = Courant()
     
     
 
