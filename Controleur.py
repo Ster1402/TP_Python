8b3c277a602b212c6545f4842e02b9f7d44836cb
TheNaitha4 <56345042+TheNaitha4@users.noreply.github.com>
2019-11-05 11:01:12 +0100

    Update Controleur.py

--- Controleur.py
+++ Controleur.py
@@ -2,3 +2,11 @@
 # Matricule : 18A911FS
 
 import Personne.py
+Class contrôleur (Personne):
+def__init__(self):
+Def vérifier (solde , montant):
+If(solde<montant):
+Print("votre solde est insuffisant pour effectuer la transaction"):
+Return false
+Else:
+Return true
