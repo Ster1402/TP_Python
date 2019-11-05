50f500fd47ead5a58d90f6da5ae6857085685c00
TheNaitha4 <56345042+TheNaitha4@users.noreply.github.com>
2019-11-05 11:54:06 +0100

    Update Controleur.py

--- Controleur.py
+++ Controleur.py
@@ -6,7 +6,7 @@ Class contrôleur (Personne):
 def__init__(self):
 Def vérifier (solde , montant):
 If(solde<montant):
-Print("votre solde est insuffisant pour effectuer la transaction"):
-Return false
+Print("votre solde est insuffisant pour effectuer la transaction")
+Return 1
 Else:
-Return true
+Return 0
