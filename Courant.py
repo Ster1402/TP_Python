1ceccdf6a153a5ded2dc8afa5497f35d3e4c63da
raoulphenix <57238018+raoulphenix@users.noreply.github.com>
2019-11-07 00:07:33 +0100

    Update Courant.py

--- Courant.py
+++ Courant.py
@@ -1,1 +1,13 @@
+#Raoul
 
+from Compte import Compte
+
+class Courant(Compte):
+    
+    def __init__(self, n = 0 , solde = 0.0):
+        Compte.__init__(self,n,solde)
+    
+    #Grâce aux comptes Courant on peut éffectué des tranferts d'argent à un autre Compte
+    def transfert ( self, compt = Compte() , montant ):
+      compt.versement(montant)
+        
