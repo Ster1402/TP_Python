f719f9d4d6bfa187193651de8ef033abe63e95f3
hmdamiral <56998075+hmdamiral@users.noreply.github.com>
2019-11-06 22:11:39 +0100

    Update Gestionnaire.py

--- Gestionnaire.py
+++ Gestionnaire.py
@@ -1,5 +1,22 @@
-#...
-#...
+# Hamidou Oumarou
+# Matricule : 18A026FS
 
-import Banque.py
+from Personne import Personne
+from Compte import Compte 
+from Courant import Courant 
+from Epargne import Epargne
+from Guichetier import Guichetier
+from Controleur import Controleur
+
+
+class Gestionnaire(Personne):
+    
+    def __init__(self,matricule = " ",nom = " ",prenom = " "):
+        Personne.__init__(self,matricule,nom,prenom);
+        
+    def ajoutCompte(self, bank ):
+        
+        bank.nbrCompte = bank.nbrCompte + 1;
+        
+        
 
