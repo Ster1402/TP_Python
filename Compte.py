dfb4620a606f32ea6220db0c60396cac7809dccf
NDE TSAPI STEVE-ROLAND <56319959+Ster1402@users.noreply.github.com>
2019-11-06 21:46:18 +0100

    Update Compte.py

--- Compte.py
+++ Compte.py
@@ -18,7 +18,7 @@ class Compte: #création classe mère Compte bancaire
         return self._numero
     
     def _get_solde(self):
-        print("Votre solde est de {0} FCFA \n".format(self._numero))
+        print("Votre solde est de {0} FCFA \n".format(self._solde))
     
     def _set_solde(self,new_solde):
         print("Votre nouveau solde est de {0} FCFA \n".format(new_solde))
@@ -39,4 +39,6 @@ class Compte: #création classe mère Compte bancaire
     
     solde = property(_get_solde, _set_solde)    
     numero = property(_get_numero)
+
+
 
