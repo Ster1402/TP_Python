f73c43b68575a3073c02bc890296024c033f2bcc
NDE TSAPI STEVE-ROLAND <56319959+Ster1402@users.noreply.github.com>
2019-11-06 21:52:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+#importation des différentes classes
+
 from Banque import Banque
+from Compte import Compte
 
 
 
 #Teste du fonctionnement de la classe Banque()
-
 banq = Banque()
 
 #Grâce à la méthode __str__ on peut faire : 
 print(banq) 
+
+#Teste de la classe Compte
+
+#La création d'un compte ce fait normalement lors de la déclaration d'un Client ( ceci est juste un exemple )
+c = Compte(156,40000)
+print(c)
