22416d3dab3a574af68513e01ddc972246c7ebb5
NDE TSAPI STEVE-ROLAND <56319959+Ster1402@users.noreply.github.com>
2019-11-08 10:07:30 +0100

    Update Courant.py

--- Courant.py
+++ Courant.py
@@ -8,7 +8,7 @@ class Courant(Compte):
         Compte.__init__(self,n,solde)
     
     #Grâce aux comptes Courant on peut éffectué des tranferts d'argent à un autre Compte
-    def transfert ( self, compt = Compte() , montant ):
+    def transfert ( self, compt , montant ):
       self._solde -= montant  
       compt.versement(montant)
         
