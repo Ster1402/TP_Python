9a97cf7e488f79c8f857535d93edb1028b910f1a
DJOUDAHACK2 <57520996+DJOUDAHACK2@users.noreply.github.com>
2019-11-08 08:50:44 +0100

    Update Epargne.py

--- Epargne.py
+++ Epargne.py
@@ -1,1 +1,14 @@
+# DJOUDA TCHOUNDA CABREL
+# Matricule : 18A043FS
 
+from Compte import Compte
+
+class Epargne(Compte):
+  def __init(self,n = 0, mt = 0.0):
+    Compte.__init__(self,n,mt)
+  
+  def consulterEpargne(self):
+    print("Votre épargne s'élève à {0} FCFA".format(self._solde))
+  
+  def __str__(self):
+    return "\tCompte Epargne\n" + Compte.__str__(self)
